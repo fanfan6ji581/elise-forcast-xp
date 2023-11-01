7ff0c06a9234d057ec60e12987f677cb405ebb73
Xinyu Li <xinyuli@uber.com>
2023-11-01 13:27:29 +1100

    update quiz page

--- src/components/attendant/QuizPage.js
+++ src/components/attendant/QuizPage.js
@@ -2,14 +2,17 @@ import { useParams } from "react-router-dom";
 import { xpConfigS } from "../../slices/gameSlice";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom"
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { Container, Box, Typography, Button, Grid, } from "@mui/material";
+import { Link } from 'react-router-dom';
 
 const QuizPage = () => {
   const { alias } = useParams()
   const navigate = useNavigate();
   const xpConfig = useSelector(xpConfigS);
+  const [showQuizBtn, setShowQuizBtn] = useState(false);
 
-  useEffect(() => {
+  const nextPage = () => {
     if (xpConfig) {
       switch (xpConfig.treatment) {
         case 3:
@@ -21,8 +24,47 @@ const QuizPage = () => {
           return navigate(`/xp/${alias}/quiz1`)
       }
     }
+  }
+
+  const onKeyDown = (e) => {
+    if (
+      (e.ctrlKey && e.key === 'm') ||
+      e.key === ' '
+    ) {
+      setShowQuizBtn(true);
+    }
+  }
+
+  useEffect(() => {
+    document.addEventListener("keydown", onKeyDown, false);
+    return () => {
+      document.removeEventListener("keydown", onKeyDown, false);
+    }
     // eslint-disable-next-line
   }, [])
+
+  return <>
+    <Container maxWidth="lg">
+      <Grid container>
+        <Grid item xs={12}>
+          <Typography textAlign="center" variant="h4" sx={{ my: 5 }}>Please wait for further instructions from the experimenters.</Typography>
+        </Grid>
+
+
+        <Grid item xs={12}>
+          {showQuizBtn &&
+            <Box textAlign="center" sx={{ my: 10 }}>
+              <Button onClick={nextPage} sx={{ width: 240, padding: 3 }} variant="contained" size="large">Start Quiz</Button>
+            </Box>
+          }
+          <Box textAlign="center" >
+            <Button component={Link} to={`/xp/${alias}/instruction-ready`} sx={{ width: 240, padding: 3 }} variant="outlined" size="large">Prev</Button>
+          </Box>
+        </Grid>
+
+      </Grid>
+    </Container>
+  </>
 };
 
 export default QuizPage;
